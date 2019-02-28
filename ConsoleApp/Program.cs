adf584155f47daf20942e58876090b50cb749783
Ago <ago.altjoe@ttu.ee>
2019-02-28 11:02:03 +0200

    Added function

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -16,34 +16,30 @@ namespace ConsoleApp
             Console.Write("Find anagrams: ");
             var anagramInput = Console.ReadLine()?.ToLower();
             var watch = Stopwatch.StartNew();
-            var anagramsList = new List<string>();
-            if (anagramInput != null)
-            {
-                var inputOrder = string.Concat(anagramInput.OrderBy(c => c));
-                var dictionary = File.ReadAllLines(path ?? throw new InvalidOperationException(), Encoding.GetEncoding("iso-8859-13"));
-                foreach (var expression in dictionary)
-                {
-                    var expressionLower = expression.ToLower();
-                    var expressionOrder = string.Concat(expressionLower.OrderBy(c => c));
-                    if (inputOrder == expressionOrder)
-                    {
-                        anagramsList.Add(expression);
-                    }
-                }
-            }
-
+            var resultList = FindAnagrams(path, anagramInput);
             var runTime = watch.Elapsed.TotalMilliseconds * 10000;
             watch.Stop();
-            if (anagramsList.Capacity == 0)
-            {
+            if (resultList.Capacity == 0)
                 Console.WriteLine(runTime + ", No anagrams found!");
-            }
             else
-            {
-                Console.WriteLine(runTime + ", " + string.Join(", ", anagramsList));
-            }
-
+                Console.WriteLine(runTime + ", " + string.Join(", ", resultList));
             Console.Read();
+        }
+
+        public static List<string> FindAnagrams(string path, string anagramInput)
+        {
+            var anagramsList = new List<string>();
+            if (anagramInput == null) return anagramsList;
+            var inputOrder = string.Concat(anagramInput.OrderBy(c => c));
+            var dictionary = File.ReadAllLines(path ?? throw new InvalidOperationException(),
+                Encoding.GetEncoding("iso-8859-13"));
+            anagramsList.AddRange(from expression in dictionary
+                let expressionLower = expression.ToLower()
+                let expressionOrder = string.Concat(expressionLower.OrderBy(c => c))
+                where inputOrder == expressionOrder
+                select expression);
+
+            return anagramsList;
         }
     }
 }
